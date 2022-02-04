f28fe0cce332d049d4a604cb8a0ed19dd292997b
Solomatov A.A <solomatov.andrew@nami.ru>
2022-02-04 12:10:22 +0400

    Import changes from 'encrypted' branch: - adding version string in startup log output; - extract versioning information in separated fiel 'console.h' with   global definitions of project.

---
+++ main/console.h
@@ -0,0 +1,32 @@
+/*   Console example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+
+   console.h
+
+   Global Definitions of Console example project
+
+   Created on: 4 февр. 2022 г.
+   Author: aso
+
+*/
+
+
+#ifndef MAIN_CONSOLE_H_
+#define MAIN_CONSOLE_H_
+
+#define quote(a) #a
+#define str(a) quote(a)
+
+#define VER_prj v.1.1.3
+#define DATE_prj 04.02.22.
+#define MODIFIER_prj aso
+
+
+
+
+#endif /* MAIN_CONSOLE_H_ */
--- main/console_example_main.c
+++ main/console_example_main.c
@@ -7,6 +7,8 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
+#include "console.h"
+
 #include <stdio.h>
 #include <string.h>
 #include "esp_system.h"
@@ -156,6 +158,7 @@ void app_main(void)
 
     printf("\n"
            "This is an example of ESP-IDF console component.\n"
+           "Version " str(VER_prj) " of " str(DATE_prj) ", modified by " str(MODIFIER_prj) ".\n"
            "Type 'help' to get the list of commands.\n"
            "Use UP/DOWN arrows to navigate through command history.\n"
            "Press TAB when typing command name to auto-complete.\n"
