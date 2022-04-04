2c0027cde4f6205d8716a397b0de91b32dc113c5
Solomatov A.A <solomatov.andrew@nami.ru>
2022-04-04 11:38:03 +0400

    Added a 'cmd_sdcard' component for operate with sd-cards respectively.

---
+++ components/cmd_sdcard/cmd_sdcard.cpp
@@ -0,0 +1,17 @@
+/*
+ * SD-card manipulations command definition
+ * Implementation file
+ *	Author:  aso (Solomatov A.A.)
+ *	Created: 04.04.2022
+ *	Version: 0.1
+ */
+
+
+#include "cmd_sdcard.hpp"
+
+#include <stdio.h>
+
+void func(void)
+{
+
+}
---
+++ components/cmd_sdcard/cmd_sdcard.hpp
@@ -0,0 +1,8 @@
+
+
+#ifndef __CMD_SDCARD__
+#define __CMD_SDCARD__
+
+void func(void);
+
+#endif /* __CMD_SDCARD__ */
