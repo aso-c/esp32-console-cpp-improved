e4f0bad73defe8b5d584146510dc8dc5fd7ac2ef
Solomatov A.A <solomatov.andrew@nami.ru>
2022-04-25 10:32:35 +0400

    - Complete syntax description developiment. - Clean the code. - Start developing SD card operaion immediately.

--- components/cmd_sdcard/cmd_sdcard.cpp
+++ components/cmd_sdcard/cmd_sdcard.cpp
@@ -13,6 +13,7 @@
 #include <stdarg.h>
 
 #include <string.h>
+#include <sys/unistd.h>
 #include "esp_log.h"
 #include "esp_console.h"
 #include "esp_system.h"
@@ -31,6 +32,9 @@
 
 //using namespace idf;
 using namespace std;
+
+
+#define MOUNT_POINT "/sdcard"
 
 
 /*
@@ -113,6 +117,173 @@ bool isempty(const char *str)
 	    return false;
     return true;
 }; /* isempty */
+
+
+
+
+
+// SD/MMC card class
+class SDMMC_card
+{
+    sdmmc_card_t *card;
+
+}; /* SDMMC_card */
+
+
+class SDMMC_host
+{
+    ;
+}; /* SDMMC_host */
+
+
+class SDMMC_slot
+{
+    ;
+}; /* SDMMC_slot */
+
+
+class SDMMC
+{
+
+    esp_err_t ret;
+
+
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+    #ifdef CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED
+        .format_if_mount_failed = true,
+    #else
+        .format_if_mount_failed = false,
+    #endif // EXAMPLE_FORMAT_IF_MOUNT_FAILED
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+
+    const char mount_point[] = MOUNT_POINT;
+    //ESP_LOGI(TAG, "Initializing SD card");
+
+}; /* SDMMC */
+
+
+
+#if 0
+
+/********** Content of the file sd_card_example.nain.c **********/
+
+
+
+void app_main(void)
+{
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+
+    ESP_LOGI(TAG, "Using SDMMC peripheral");
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+
+    // To use 1-line SD mode, change this to 1:
+    slot_config.width = 4;
+
+    // On chips where the GPIOs used for SD card can be configured, set them in
+    // the slot_config structure:
+#ifdef SOC_SDMMC_USE_GPIO_MATRIX
+    slot_config.clk = GPIO_NUM_14;
+    slot_config.cmd = GPIO_NUM_15;
+    slot_config.d0 = GPIO_NUM_2;
+    slot_config.d1 = GPIO_NUM_4;
+    slot_config.d2 = GPIO_NUM_12;
+    slot_config.d3 = GPIO_NUM_13;
+#endif
+
+    // Enable internal pullups on enabled pins. The internal pullups
+    // are insufficient however, please make sure 10k external pullups are
+    // connected on the bus. This is for debug / example purpose only.
+    slot_config.flags |= SDMMC_SLOT_FLAG_INTERNAL_PULLUP;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    // Use POSIX and C standard library functions to work with files:
+
+    // First create a file.
+    const char *file_hello = MOUNT_POINT"/hello.txt";
+
+    ESP_LOGI(TAG, "Opening file %s", file_hello);
+    FILE *f = fopen(file_hello, "w");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return;
+    }
+    fprintf(f, "Hello %s!\n", card->cid.name);
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    const char *file_foo = MOUNT_POINT"/foo.txt";
+
+    // Check if destination file exists before renaming
+    struct stat st;
+    if (stat(file_foo, &st) == 0) {
+        // Delete it if it exists
+        unlink(file_foo);
+    }
+
+    // Rename original file
+    ESP_LOGI(TAG, "Renaming file %s to %s", file_hello, file_foo);
+    if (rename(file_hello, file_foo) != 0) {
+        ESP_LOGE(TAG, "Rename failed");
+        return;
+    }
+
+    // Open renamed file for reading
+    ESP_LOGI(TAG, "Reading file %s", file_foo);
+    f = fopen(file_foo, "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+
+    // Read a line from file
+    char line[64];
+    fgets(line, sizeof(line), f);
+    fclose(f);
+
+    // Strip newline
+    char *pos = strchr(line, '\n');
+    if (pos) {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Read from file: '%s'", line);
+
+    // All done, unmount partition and disable SDMMC peripheral
+    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    ESP_LOGI(TAG, "Card unmounted");
+}
+
+#endif
+
+
 
 
 
@@ -331,11 +502,6 @@ int SDctrl::act_mnt()
     }; /* switch argc */
     cout << endl;
 
-//    cout << "args number : " << argc << endl;
-//    cout << "args is     : " << argc << endl;
-//    for (int i = 0; i < argc; i++)
-//	cout << "argv[" << i << "] is  : " << argv[i] << endl;
-
     return 0;
 }; /* SDctrl::act_mnt */
 
@@ -507,7 +673,7 @@ void** SDctrl::Syntax::tables()
 	};
     // syntax2: u | umount [ <device> | <mountpoint> ] "unmount SD-card <device> or that was mounted to <path>; if all parameters omitted - use default values - ..."
 	static void* arg_umnt[] = {
-		arg_rex1(NULL, NULL, "u|umount", NULL, 0, "unmount SD-card <device> or <path> where the SD card is mounted; if parameters omitted - use default values - ..."),
+		arg_rex1(NULL, NULL, "u|umount", NULL, 0, "unmount SD-card <device> or <path> where the SD card is mounted; if parameters omitted - use \"" MOUNT_POINT "\"" ),
 		arg_rem ("[", NULL),
 		arg_str1(NULL, NULL, "<device>", "SD card device name, used default value if omitted"),
 		arg_rem ("|", NULL),
@@ -605,8 +771,6 @@ int SDctrl::Syntax::help()
 	cout << "       " << parent.argv[0];
 	arg_print_syntax(stdout, (void**)*currcmd, "\n");
     }; /* for void **currcmd */
-//    cout << "       " << parent.argv[0];
-//    arg_print_syntax(stdout, (void**)alltables[0], "\n");
 
     cout << "Command \"" << parent.argv[0] << "\" supports the ESP32 operation with an SD card." << endl;
     cout << "Use subcommands to invoke individual operations; operation are: mount, unmount, ls, cat, type, help." << endl;
