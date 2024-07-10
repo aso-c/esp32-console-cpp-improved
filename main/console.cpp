259556fd56ae6afc6117f1b259e20e9b876181cc
Solomatov A.A <solomatov.andrew@nami.ru>
2024-07-10 14:27:57 +0400

    Implement the trim() procedure, check it's work. Improve methods of CWD_emulation class with std::string type. Implement buffer CWD_emulation::pwd with type std::string. Implement small wrapper freewrapper for pointers to dinamically allocated memory and dispose it with the free() procedure when the wariable out of scope.

--- main/console.cpp
+++ main/console.cpp
@@ -32,6 +32,8 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 
+#include <astring.h>
+
 //using namespace idf;
 using namespace std;
 
@@ -342,6 +344,12 @@ extern "C" void app_main(void)
         prompt = PROMPT_STR "> ";
 #endif //CONFIG_LOG_COLORS
     }
+
+    std::string test1 = "   3front_space";
+    std::string test2 = "   3front_5tail_space     ";
+    ESP_LOGW("astr::trim_check", "Trimming check1, orig: [%s], trimmed: [%s]", test1.c_str(), astr::trim(test1).c_str());
+    ESP_LOGW("astr::trim_check", "Trimming check2, orig: [%s], trimmed: [%s]", test2.c_str(), astr::trim(test2).c_str());
+    ESP_LOGW("astr::trim_check", "Check the Trimm 3, 5 spaces, orig: [%s], trimmed: [%s]", "  \t   ", astr::trim("  \t   ").c_str());
 
     /* Main loop */
     while(true) {
